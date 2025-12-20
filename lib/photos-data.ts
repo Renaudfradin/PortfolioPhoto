421ab7c50061de489eaaa4b7023d46ef3f3049cd
Renaudfradin <renaudfradin@gmail.com>
2025-12-20 14:27:25 +0100

    (update) add new picture

--- lib/photos-data.ts
+++ lib/photos-data.ts
@@ -1,10 +1,15 @@
-import img35mn_003 from '@/app/assets/img/35mn/00002070-003.jpg';
-import img35mn_013 from '@/app/assets/img/35mn/00002070-013.jpg';
-import img35mn_015 from '@/app/assets/img/35mn/00002070-015.jpg';
-import img35mn_002 from '@/app/assets/img/35mn/00002071-002.jpg';
-import img35mn_003_2 from '@/app/assets/img/35mn/00002071-003.jpg';
-import img35mn_018 from '@/app/assets/img/35mn/00002071-018.jpg';
-import img35mn_023 from '@/app/assets/img/35mn/00002071-023.jpg';
+import img35mn_00002070_003 from '@/app/assets/img/35mn/00002070-003.jpg';
+import img35mn_00002070_013 from '@/app/assets/img/35mn/00002070-013.jpg';
+import img35mn_00002070_015 from '@/app/assets/img/35mn/00002070-015.jpg';
+import img35mn_00002071_002 from '@/app/assets/img/35mn/00002071-002.jpg';
+import img35mn_00002071_003 from '@/app/assets/img/35mn/00002071-003.jpg';
+import img35mn_00002071_018 from '@/app/assets/img/35mn/00002071-018.jpg';
+import img35mn_00002071_023 from '@/app/assets/img/35mn/00002071-023.jpg';
+import img35mn_00003229_007 from '@/app/assets/img/35mn/00003229-007.jpg';
+import img35mn_00003229_023 from '@/app/assets/img/35mn/00003229-023.jpg';
+import img35mn_00003229_030 from '@/app/assets/img/35mn/00003229-030.jpg';
+import img35mn_00003229_035 from '@/app/assets/img/35mn/00003229-035.jpg';
+import img35mn_00003229_037 from '@/app/assets/img/35mn/00003229-037.jpg';
 import img110mn_001 from '@/app/assets/img/110mn/001.jpg';
 import img110mn_006 from '@/app/assets/img/110mn/006.jpg';
 import img110mn_011 from '@/app/assets/img/110mn/011.jpg';
@@ -21,60 +26,60 @@ export interface PhotoData {
 
 export const photos: PhotoData[] = [
   {
-    src: img35mn_003,
+    src: img35mn_00002070_003,
     alt: 'Photographie argentique 35mm - 00002070-003',
     series: '35mm',
     date: '2025',
-    id: '35mn-003',
+    id: '35mn_00002070_003',
     city: 'Rivière-du-Loup',
   },
   {
-    src: img35mn_013,
+    src: img35mn_00002070_013,
     alt: 'Photographie argentique 35mm - 00002070-013',
     series: '35mm',
     date: '2025',
-    id: '35mn-013',
+    id: '35mn_00002070_013',
     city: 'Rivière-du-Loup',
   },
   {
-    src: img35mn_015,
+    src: img35mn_00002070_015,
     alt: 'Photographie argentique 35mm - 00002070-015',
     series: '35mm',
     date: '2025',
-    id: '35mn-015',
+    id: '35mn_00002070_015',
     city: 'Rivière-du-Loup',
   },
 
   {
-    src: img35mn_003_2,
+    src: img35mn_00002071_003,
     alt: 'Photographie argentique 35mm - 00002071-003',
     series: '35mm',
     date: '2025',
-    id: '35mn-003-2',
+    id: '35mn_00002071_003',
     city: 'Montreal',
   },
   {
-    src: img35mn_018,
+    src: img35mn_00002071_018,
     alt: 'Photographie argentique 35mm - 00002071-018',
     series: '35mm',
     date: '2025',
-    id: '35mn-018',
+    id: '35mn_00002071_018',
     city: 'Montreal',
   },
   {
-    src: img35mn_023,
+    src: img35mn_00002071_023,
     alt: 'Photographie argentique 35mm - 00002071-023',
     series: '35mm',
     date: '2025',
-    id: '35mn-023',
+    id: '35mn_00002071_023',
     city: 'Montreal',
   },
   {
-    src: img35mn_002,
+    src: img35mn_00002071_002,
     alt: 'Photographie argentique 35mm - 00002071-002',
     series: '35mm',
     date: '2025',
-    id: '35mn-002',
+    id: '35mn_00002071_002',
     city: 'Montreal',
   },
   {
@@ -109,4 +114,50 @@ export const photos: PhotoData[] = [
     id: '110mn-019',
     city: 'Rivière-du-Loup',
   },
+  
+  {
+    src: img35mn_00003229_007,
+    alt: 'Photographie argentique 35mm - 00003229-007',
+    series: '35mm',
+    date: '2025',
+    id: '35mn_00003229_007',
+    city: 'Nice',
+  },
+  
+  {
+    src: img35mn_00003229_023,
+    alt: 'Photographie argentique 35mm - 00003229-023',
+    series: '35mm',
+    date: '2025',
+    id: '35mn_00003229_023',
+    city: 'Nice',
+  },
+  
+  {
+    src: img35mn_00003229_030,
+    alt: 'Photographie argentique 35mm - 00003229-030',
+    series: '35mm',
+    date: '2025',
+    id: '35mn_00003229_030',
+    city: 'Nice',
+  },
+  
+  {
+    src: img35mn_00003229_035,
+    alt: 'Photographie argentique 35mm - 00003229-035',
+    series: '35mm',
+    date: '2025',
+    id: '35mn_00003229_035',
+    city: 'Nice',
+  },
+  
+  {
+    src: img35mn_00003229_037,
+    alt: 'Photographie argentique 35mm - 00003229-037',
+    series: '35mm',
+    date: '2025',
+    id: '35mn_00003229_037',
+    city: 'Paris',
+  },
+  
 ];
